f2778f8cd75057ee5ce3227ade9f20d40b260854
Gfrey <gfreycode@gmail.com>
2023-05-05 10:38:26 -0500

    remove: proof

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,7 @@
-import Proof from "@/components/Proof/Proof";
-import Proof2 from "@/components/Proof2/Proof2";
-
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Proof />
-      <Proof2 />
     </div>
   );
 }
