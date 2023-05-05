ac46fa78187642eea7d7837f794c853d9ffe2382
Adri-ESM <anggivmorales@gmail.com>
2023-05-05 17:58:46 -0400

    add new team member, images of members and styles

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,28 +2,40 @@ import style from './about.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 
+
 export default function Page() {
   const team = [
     {
       name: 'Jefferson',
+      image: 'jef.png'
     },
     {
       name: 'Yael',
+      image: 'yael.png'
     },
     {
       name: 'Franco',
+      image: 'franco.png'
     },
     {
       name: 'Axel',
+      image: 'axel.png'
     },
     {
       name: 'Jean',
+      image: 'jean.png'
     },
     {
       name: 'Emmanuel',
+      image: 'ema.png'
     },
     {
       name: 'Adriana',
+      image: 'adri.png'
+    },
+    {
+      name: 'Celes',
+      image: 'celes.png'
     },
     
   ]
@@ -36,21 +48,39 @@ export default function Page() {
           Una web application que ofrece el servicio de Alquiler y venta
           de herramientas usadas entre vecinos es una plataforma en
           línea que permite a los usuarios publicar y buscar herramientas
-          que estén disponibles para alquilar o vender dentro de su vecindario o comunidad cercana.
+          que estén disponibles para alquilar o vender dentro de su vecindario 
+          o comunidad cercana.
+          <br/>
+          <br/>
+          Somos un equipo de profesionales apasionados por el desarrollo de herramientas 
+          tecnológicas que mejoren la vida de las personas. Trabajamos juntos para crear 
+          soluciones innovadoras y eficaces que resuelvan los desafíos actuales del 
+          mundo digital.
           </p>
       </div>
       <div className={style.aboutCardContainer}>
         {team.map(person => (
           <div key={person.name} className={style.cardInfoTeam}>
+              <Image 
+                src={`/images-view-about/${person.image}`} 
+                width={500} 
+                height={500} 
+                alt={`Picture of ${person.name}`}
+                className={style.aboutImage}
+              />
               <h2 className={style.aboutName}>{person.name}</h2>
               <div className={style.aboutLinks}>
-                <Link href='/'>github</Link>
-                <Link href='/'>linkedin</Link>
+                <a href='https://github.com' target='_blanket'>
+                  <img className={style.github} src='/images-view-about/github.png' alt='GitHub' />
+                </a>
+                <a href='https://linkedin.com' target='_blanket'>
+                  <img className={style.linkedin} src='/images-view-about/linkedin.png' alt='LinkedIn' />
+                </a>
               </div>
           </div>
         ))}
       </div>
     </div>
-    );
-  }
+  );
+}
 
