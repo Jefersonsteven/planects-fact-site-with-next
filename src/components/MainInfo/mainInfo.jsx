09ba2d516231fa7e69e6117ee021d7881e287ef2
Rai5559 <icexbee1@gmail.com>
2023-05-05 20:10:16 -0600

    Receive props, rendering: Image, Info, Button's section and giving props/YRH

--- src/components/MainInfo/mainInfo.jsx
+++ src/components/MainInfo/mainInfo.jsx
@@ -1,9 +1,15 @@
-const MainInfo = () => {
-  <section>
-    <div></div>
-    <div></div>
-    <div></div>
-  </section>;
+import Section2 from "../Section-2/Section-2";
+import { MainSectionImage } from "../mainSectionImage/MainSectionImage";
+import { MainSectionInfo } from "../mainSectionInfo/MainSectionInfo";
+
+const MainInfo = ({ info }) => {
+  return (
+    <section>
+      <MainSectionImage image={info.images.planet} />
+      <MainSectionInfo info={info} />
+      <Section2 info={info} />
+    </section>
+  );
 };
 
 export default MainInfo;
