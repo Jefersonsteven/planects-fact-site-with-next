10af3d53a7cccd87bf94da3e1d24767bd48a8257
Rai5559 <icexbee1@gmail.com>
2023-05-05 20:11:56 -0600

    To fix: Adding 'alt' attribute, the image does not works/YRH

--- src/components/mainSectionImage/MainSectionImage.jsx
+++ src/components/mainSectionImage/MainSectionImage.jsx
@@ -1,13 +1,11 @@
-import Image from "next/image"
+import Image from "next/image";
 
-export function MainSectionImage ({image}) {
-    return (
-        <div>
-        <figure>
-            <Image width={300} height={300} src={image}/>
-        </figure>
-        </div>
-    );
-};
-
-
+export function MainSectionImage({ image }) {
+  return (
+    <div>
+      <figure>
+        <Image width={300} height={300} src={image} alt="nan" />
+      </figure>
+    </div>
+  );
+}
