b80b888fc16b48cba27c92796503d7417aaef614
Rai5559 <icexbee1@gmail.com>
2023-05-05 20:07:22 -0600

    Deleted section 2, Main info added with props to render: info-image, info-planet, info-buttons

--- src/app/page.js
+++ src/app/page.js
@@ -2,26 +2,22 @@
 
 import Footer from "@/components/Footer/footer";
 import MainInfo from "@/components/MainInfo/mainInfo";
-import Image from "next/image";
-import Section2 from "@/components/Section-2/Section-2";
 import data from "../data.json";
 import { useState } from "react";
 
 export default function Home() {
-  const [planetInfo, setPlanetInfo] = useState(data);
-  const [planetName, setPlanetName] = useState("Mercury");
+  const [planetInfo] = useState(data);
+  const [planetName] = useState("Mercury");
   return (
     <div>
-      <Section2
-        key={planetInfo[planetName]}
-        info={planetInfo.find((planet) => planet.name === planetName)}
-      />
-
       {/*       {planetsInfo.map((planet) => {
         return <Section2 key={planet.name} info={planet} />;
       })} */}
       <h1>Home</h1>
-      <MainInfo />
+      <MainInfo
+        key={planetInfo[planetName]}
+        info={planetInfo.find((planet) => planet.name === planetName)}
+      />
       <Footer />
     </div>
   );
